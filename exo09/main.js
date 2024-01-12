35dc5cb3b355e87217f97c3a717632859d21330f
kferrerux <ferrerkevin@hotmail.fr>
2024-01-12 10:10:45 +0100

    exo09 added

---
+++ exo09/main.js
@@ -0,0 +1,12 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/style
+// https://developer.mozilla.org/fr/docs/Web/API/HTMLStyleElement
+
+let element = document.getElementById('box');
+
+element.style.fontSize = '2rem';
+element.style.width = '300px';
+element.style.margin = '0 auto';
+element.style.padding = '5%';
+element.style.backgroundColor = 'green';
+element.style.color = 'white';
