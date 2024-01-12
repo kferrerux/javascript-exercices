aa42b29430ce66400e5baea82fb69bb00d6375d4
kferrerux <ferrerkevin@hotmail.fr>
2024-01-12 20:41:40 +0100

    exo10 update

--- exo10/main.js
+++ exo10/main.js
@@ -3,10 +3,22 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Element/setAttribute
 
 let element = document.getElementById('image');
-// console.log(element);
-
-let attribute = element.getAttribute('src');
-// console.log(attribute);
-
-element.setAttribute('src', 'assets/imgs/chat02.png');
 console.log(element);
+
+image.getAttribute('src');
+console.log(image);
+
+image.setAttribute('src', 'assets/imgs/chat02.png');
+console.log(image);
+
+
+// DIfférentes façons de manipuler les attributs
+
+// images = document.body.getElementsByTagName('img');
+// console.log(images);
+
+// let src0 = images[5].getAttribute('src');
+// console.log(src0);
+
+// let src2 = images[1].src;
+// console.log(src2);
